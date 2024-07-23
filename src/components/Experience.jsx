0f443d82884fc191ae83f98f590ea518893ede59
hrushii <hrushiop@gmail.com>
2024-07-23 17:21:35 +0530

    added models

---
+++ src/components/Experience.jsx
@@ -0,0 +1,64 @@
+import { Float, Line, OrbitControls } from "@react-three/drei";
+import Background from "./Background";
+import { Airplane } from "./Airplane";
+import { Cloud } from "./Cloud";
+import { useMemo } from "react";
+import * as THREE from "three";
+export const Experience = () => {
+  const LINE_NB_POINTS = 200;
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+        new THREE.Vector3(0, 0, -80),
+        new THREE.Vector3(0, 0, -90),
+        new THREE.Vector3(0, 0, -100),
+      ],
+      false,
+      "catmullrom",
+      0.5
+    );
+  }, []);
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  }, [curve]);
+  return (
+    <>
+      <OrbitControls />
+      <Background />
+      <Float floatIntensity={2} speed={2}>
+        <Airplane
+          rotation-y={Math.PI / 2}
+          scale={[0.2, 0.2, 0.2]}
+          position-y={0.1}
+        />
+      </Float>
+      <group position-y={-2}>
+        <Line points={linePoints} color={"white"} opacity={0.7}  transparent lineWidth={16}/>
+      </group>
+      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 2, -3]} />
+      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
+      <Cloud
+        opacity={0.7}
+        scale={[0.3, 0.3, 0.4]}
+        rotation-y={Math.PI / 9}
+        position={[2, -0.2, -2]}
+      />
+      <Cloud
+        opacity={0.7}
+        scale={[0.4, 0.4, 0.4]}
+        rotation-y={Math.PI / 9}
+        position={[2, -0.2, -12]}
+      />
+      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]} />
+    </>
+  );
+};
